635b63a518beb5f7ff6b13bf8dcba82ef475763f
Cielo <macielo.tablate@gmail.com>
2022-10-13 17:51:32 +0200

    vidly proj

--- 02-vidly-proj/src/components/movies.jsx
+++ 02-vidly-proj/src/components/movies.jsx
@@ -6,37 +6,59 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  fStyle = {
+    paddingTop: 30,
+    fontWeight: 'bold',
+    fontSize: '2rem',
+    color: '#00ff00'
+  }
+
   handleDelete = (movie) => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id );
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
     // this.setState({movies: movies}) OR when key & value are the same
-    this.setState({movies})
+    this.setState({ movies });
   };
 
   render() {
+    const { length: count} = this.state.movies;
+
+    if (count === 0)
+      return <p style={{paddingTop: 30, fontWeight: 'bold', fontSize: '2rem', color: '#00ff00'}}>There are no movies in the database.</p>;
     return (
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-            <th><i className='fa fa-trash-o'></i></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
+      <>
+        <p style={this.fStyle}>Showing { count } movie/s in the database.</p>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>
+                <i className='fa fa-trash-o'></i>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className='btn btn-danger btn-sm'
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
     );
   }
 }
